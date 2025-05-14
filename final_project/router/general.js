351256698cb073f5af896b9cf6dac282dbfec304
SaiC4 <saichandra604@gmail.com>
2025-05-14 19:52:54 -0400

    task 4 complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -55,13 +55,36 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLowerCase();
+  const bookKeys = Object.keys(books);
+  const matchingBooks = [];
+
+  for (let key of bookKeys) {
+    if (books[key].title.toLowerCase() === title) {
+      matchingBooks.push({ isbn: key, ...books[key] });
+    }
+  }
+
+  if (matchingBooks.length > 0) {
+    return res.status(200).json({ booksByTitle: matchingBooks });
+  } else {
+    return res.status(404).json({ message: "No books found with that title" });
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  const book = books[isbn];
+  if (book) {
+    return res.status(200).json({ reviews: book.reviews });
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
