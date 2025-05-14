725cea14ac79f9c48f9939fa6803eb335a39a62f
SaiC4 <saichandra604@gmail.com>
2025-05-14 19:47:31 -0400

    task 3 completel

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -34,7 +34,22 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const bookKeys = Object.keys(books);
+  const matchingBooks = [];
+
+  for (let key of bookKeys) {
+    if (books[key].author.toLowerCase() === author.toLowerCase()) {
+      matchingBooks.push({ isbn: key, ...books[key] });
+    }
+  }
+
+  if (matchingBooks.length > 0) {
+    return res.status(200).json({ booksByAuthor: matchingBooks });
+  } else {
+    return res.status(404).json({ message: "No books found for this author" });
+  }
 });
 
 // Get all books based on title
